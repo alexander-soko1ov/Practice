776af98c8585d33c80639879fced69c4d8386fa0
alexander-soko1ov <alex.sokolov@internet.ru>
2022-07-13 01:52:01 +0300

    Эхо телеграм бот

---
+++ telegbot.py
@@ -0,0 +1,17 @@
+import telebot
+
+# Создаем экземпляр бота
+bot = telebot.TeleBot('5337642115:AAGdiECk6iqFXoAV2n9Cq31WTywXyRtvZVc')
+
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
