c2c0119bcc96ac79b5bdf3c200e28b396c14f1b8
alexander-soko1ov <alex.sokolov@internet.ru>
2022-07-13 02:36:34 +0300

    Выделение времени из строки

--- regular expressions.py
+++ regular expressions.py
@@ -1,7 +1,9 @@
 import re
 
-text = "Карта map и объект bitmap - это разные вещи"
+# text = "Карта map и объект bitmap - это разные вещи"
+# result = re.findall(r"\bmap\b", text)
 
-result = re.findall(r"\bmap\b", text)
+text = "Еда, беду, победа, 25:15, 6, 1:12"
+result = re.findall(r"[0-9]?[0-9]:\d{2}", text)
 
 print(result)
